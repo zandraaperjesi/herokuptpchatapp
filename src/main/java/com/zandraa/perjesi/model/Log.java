b3e290f31831faaa46e47305f0085218a1345e67
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-05-17 11:26:19 +0200

    add database

---
+++ src/main/java/com/zandraa/perjesi/model/Log.java
@@ -0,0 +1,64 @@
+package com.zandraa.perjesi.model;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Log {
+  private String path;
+  private String method;
+  private String dateAndTime;
+  private String logLevel;
+  private String requestData;
+
+  public Log() {}
+
+  public Log(String path, String method, String logLevel, String requestData) {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+    LocalDateTime dateTime = LocalDateTime.now();
+    dateAndTime = dateTime.format(formatter);
+    this.path = path;
+    this.method = method;
+    this.logLevel = logLevel;
+    this.requestData = requestData;
+  }
+
+  public String getPath() {
+    return path;
+  }
+
+  public void setPath(String path) {
+    this.path = path;
+  }
+
+  public String getMethod() {
+    return method;
+  }
+
+  public void setMethod(String method) {
+    this.method = method;
+  }
+
+  public String getDateAndTime() {
+    return dateAndTime;
+  }
+
+  public void setDateAndTime(String dateAndTime) {
+    this.dateAndTime = dateAndTime;
+  }
+
+  public String getLogLevel() {
+    return logLevel;
+  }
+
+  public void setLogLevel(String logLevel) {
+    this.logLevel = logLevel;
+  }
+
+  public String getRequestData() {
+    return requestData;
+  }
+
+  public void setRequestData(String requestData) {
+    this.requestData = requestData;
+  }
+}
