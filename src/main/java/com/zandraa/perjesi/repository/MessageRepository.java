4fdd84750877cf187a8fe12d0f4fd3ad53c01de4
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-05-18 16:29:52 +0200

    add messages to app

--- src/main/java/com/zandraa/perjesi/repository/MessageRepository.java
+++ src/main/java/com/zandraa/perjesi/repository/MessageRepository.java
@@ -2,6 +2,8 @@ package com.zandraa.perjesi.repository;
 
 import com.zandraa.perjesi.model.Message;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MessageRepository extends CrudRepository<Message, Long>{
 }
